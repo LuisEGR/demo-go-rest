03a4a93c35b61543d1ffe77b1c80ba4e4a8a40bb
Luis E. González <luisegr149@gmail.com>
2019-12-19 13:58:26 -0600

    echo framework

--- main.go
+++ main.go
@@ -1,75 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
+	// "github.com/labstack/echo/v4/middlewareandler"
+
+	"go-template/api"
 )
 
-func post(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"message": "post called"}`))
-}
-
-func put(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte(`{"message": "put called"}`))
-}
-
-func delete(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "delete called"}`))
-}
-
-func params(w http.ResponseWriter, r *http.Request) {
-	pathParams := mux.Vars(r)
-	w.Header().Set("Content-Type", "application/json")
-
-	userID := -1
-	var err error
-	if val, ok := pathParams["userID"]; ok {
-		userID, err = strconv.Atoi(val)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(`{"message": "need a number"}`))
-			return
-		}
-	}
-
-	commentID := -1
-	if val, ok := pathParams["commentID"]; ok {
-		commentID, err = strconv.Atoi(val)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(`{"message": "need a number"}`))
-			return
-		}
-	}
-
-	query := r.URL.Query()
-	location := query.Get("location")
-
-	w.Write([]byte(fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": "%s" }`, userID, commentID, location)))
-}
-
 func main() {
-	r := mux.NewRouter()
+	// Echo instance
+	e := echo.New()
 
-	api := r.PathPrefix("/api/v1").Subrouter()
-	api.HandleFunc("", getHandler).Methods(http.MethodGet)
-	api.HandleFunc("", post).Methods(http.MethodPost)
-	api.HandleFunc("", put).Methods(http.MethodPut)
-	api.HandleFunc("", delete).Methods(http.MethodDelete)
+	// Middleware
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
 
-	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
+	// Routes
+	e.GET("/", hello)
 
-	println("Serving on port 8080")
+	e.GET("/db", api.GetDBHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
